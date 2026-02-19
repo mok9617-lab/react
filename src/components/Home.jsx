703a5840db1ad0b6fa86804e5dcc629d3eca071c
mok <mok9617@gmail.com>
2026-02-19 10:46:52 +0900

    홈 수정

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home-card">
+        <h1>Welcome 👋</h1>
+        <p>React Router 연습</p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
